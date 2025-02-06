115762b42532031bf434b089900fc3c2894a1bb2
hossainsmshakib <smsakib2403@gmail.com>
2025-02-06 21:30:58 +0600

    Added custom datatype

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"net/http/httputil"
+)
 
-func main() {
-    fmt.Println("Hello, World!")
+type simpleServer struct {
+    address string
+    proxy httputil.ReverseProxy
 }
